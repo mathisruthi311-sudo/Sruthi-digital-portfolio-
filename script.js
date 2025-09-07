0b23e3a57bfa13794bbcf4e1faa7588f595a4c61
mathisruthi311-sudo <mathisruthi311@gmail.com>
2025-09-07 20:17:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Function to show a message on button click
+
+function showMessage() {
+
+  const messageElement = document.getElementById('message');
+
+  messageElement.textContent = "Thank you for visiting my portfolio! Keep exploring to know more about me.";
+
+}
+
